9f0b3cd1cbdada4f778f9b37b5c0a8ba9dd14320
Repits Márk <repitsm@gmail.com>
2021-11-18 23:01:34 +0100

    base of the backend with a working API endpoint

---
+++ backend/Backend-Notes.md
@@ -0,0 +1,9 @@
+# Backend ideas
+
+## API endpoints
+- /number : GET : get number from file './data/number-store.txt'
+- /number : POST : (inputdata String) write number to file (./data/number-store.txt) as string
+
+
+
+~ 2h of work the backend
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: './.env'})
+const port = process.env.PORT || 6600;
+
+app.use(bodyParser.json())
+app.route('/number')
+.get(function(req, res){
+	fs.readFile('data/number-store.txt','utf-8', function (err,data){
+		if (err){
+			res.send(err);
+		}
+		res.json({"number": data});
+	});
+})
+.post(function(req, res){
+	const newNumber = req.body.num;
+	fs.writeFile('data/number-store.txt', newNumber, err => {
+		if (err) {
+		  console.error(err)
+		  res.json({"response": "error"});
+		  return
+		}
+		res.json({"response": "success"})
+	  });
+});
+
+
+app.listen(port, () => {
+	console.log("Server is Running on port " + port);
+});
